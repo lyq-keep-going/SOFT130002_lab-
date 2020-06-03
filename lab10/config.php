726bd886cc23c9971b3e9f2d5029745e1b82c420
lyq-keep-going <1258648285@qq.com>
2020-06-03 19:54:53 +0800

    submit lab10

---
+++ lab10/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'luoyuqi');
+define('DBPASS', 'luoyuqi');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;');
+?>
